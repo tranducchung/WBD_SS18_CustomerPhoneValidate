c140749d5bd87342a253d6c8f002779b5a2b9364
tranchung <tranducchung1st@gmail.com>
2019-05-12 21:27:48 +0700

    Finish validation phone number of customer signUp

---
+++ src/main/java/com/codegym/controller/PhoneController.java
@@ -0,0 +1,32 @@
+package com.codegym.controller;
+
+import com.codegym.model.PhoneNumber;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+
+@Controller
+public class PhoneController {
+    @GetMapping("/")
+    public ModelAndView showForm(){
+        return new ModelAndView("index","phoneNumber",new PhoneNumber());
+    }
+
+    @PostMapping("/")
+    public String checkValidation(@Valid @ModelAttribute("phoneNumber") PhoneNumber phoneNumber, BindingResult bindingResult, Model model){
+        new PhoneNumber().validate(phoneNumber,bindingResult);
+        if(bindingResult.hasFieldErrors()){
+            return "index";
+        }else {
+            model.addAttribute("phoneNumber",phoneNumber.getNumber());
+            return "result";
+        }
+    }
+}
